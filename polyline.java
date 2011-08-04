983b202398d4e6717cc662ffca23d4e2979d8bff
Brijesh Chauhan <chauhan.brijesh@gmail.com>
2011-08-04 15:11:14 +0530

    CFX JAVA based TAG

---
+++ polyline.java
@@ -0,0 +1,20 @@
+import com.allaire.cfx.* ;
+public class polyline implements CustomTag {
+        public void processRequest( Request request, Response response ) 
+    throws Exception { 
+            int num = request.getIntAttribute("COORDINATE");
+            num = (int)(Math.round(num * 1e5));
+            int sgn_num = num << 1; 
+            if (num < 0) { 
+                sgn_num = ~(sgn_num); 
+            } 
+            StringBuffer encodeString = new StringBuffer();
+            while (sgn_num >= 0x20) 
+		   	{ 
+            	encodeString.append((char)((0x20 | (sgn_num & 0x1f)) + 63)); 
+            	sgn_num >>= 5; 
+        	} 
+       	encodeString.append((char)(sgn_num + 63));
+    response.write(encodeString.toString()) ; 
+    } 
+}
